3576981cde093a335e541cb39f06d2185e2f7d8b
6u5t4v05ouz4 <n4r1g4@proton.me>
2025-12-17 18:42:08 -0300

    update: new boardsize 25x25

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,5 @@
-export const BOARD_WIDTH = 35
-export const BOARD_HEIGHT = 35
+export const BOARD_WIDTH = 25
+export const BOARD_HEIGHT = 25
 export const INITIAL_SPEED = 100; // ms per tick - Keeping for types/reference, but logic overrides
 export const START_GAME_SPEED = 200; // Start slow
 export const SPEED_DECREMENT = 5; // -5ms per point
