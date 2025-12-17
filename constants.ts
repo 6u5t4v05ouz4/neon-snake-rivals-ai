5ce47525d6a1171899d0283e3f4979959d65b210
6u5t4v05ouz4 <n4r1g4@proton.me>
2025-12-17 18:51:09 -0300

    update: new boardsize 20x20

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
-export const BOARD_WIDTH = 25;
-export const BOARD_HEIGHT = 25;
+export const BOARD_WIDTH = 20;
+export const BOARD_HEIGHT = 20;
 export const SERVER_URL = 'https://neon-snake-rivals-ai-production.up.railway.app'; // Production URL
 export const INITIAL_SPEED = 100; // ms per tick - Keeping for types/reference, but logic overrides
 export const START_GAME_SPEED = 200; // Start slow
